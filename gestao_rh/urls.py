7ab17824a94247d342502b4cc810f8aaa7ff762c
André Ramos <andre_ramosdovale@outlook.com>
2021-02-21 04:48:28 -0300

    criação de formulario de criação e edição de empresa

--- gestao_rh/urls.py
+++ gestao_rh/urls.py
@@ -5,9 +5,9 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', include('apps.core.urls')),
     path('admin/', admin.site.urls),
-    path('funcionarios/', include('apps.funcionarios.urls')),
-    path('accounts/', include('django.contrib.auth.urls')),
+    path('funcionario/', include('apps.funcionarios.urls')),
+    path('account/', include('django.contrib.auth.urls')),
     # path('departamento/', include('apps.departamento.urls')),
-    # path('empresas/', include('apps.empresas.urls')),
+    path('empresa/', include('apps.empresas.urls')),
     # path('registro_hora_extra/', include('apps.registro_hora_extra.urls')),
 ]
